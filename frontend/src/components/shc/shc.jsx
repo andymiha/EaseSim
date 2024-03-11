fe9e05720338900780f0e4c46bfed36303893a25
nouratabbara <nouratabara@gmail.com>
2024-03-11 15:58:24 -0400

    added shcTab functionalities

--- frontend/src/components/shc/shc.jsx
+++ frontend/src/components/shc/shc.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+// import React from 'react';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import ItemList from '/Users/noura/Desktop/EaseSim/frontend/src/components/shc/ItemList.jsx'; 
+
+
+
 
 const SHC = () => {
   // Add your house layout content here
   return (
-    <div>
-      <h2>SHC content</h2>
-      {/* Add your house layout components/content here */}
-    </div>
+    <Box sx={{ width: 1 }}>
+    <Stack   
+      direction="column"
+      justifyContent="space-evenly"
+      alignItems= "center"
+      spacing={2}>
+      <ButtonGroup variant="outlined">
+        <Button>Lights</Button>
+        <Button>Doors</Button>
+        <Button>Windows</Button>
+      </ButtonGroup>
+      <ItemList/>
+    </Stack>
+  </Box>
   );
 };
 
