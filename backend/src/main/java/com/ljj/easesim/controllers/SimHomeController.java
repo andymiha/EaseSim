47f2b439a08bb89f8f317a3d6bb7810b9038610f
Jonathan <jonathandominicpasquin@gmail.com>
2024-03-13 21:46:05 -0400

    Staging changes for SHC

--- backend/src/main/java/com/ljj/easesim/controllers/SimHomeController.java
+++ backend/src/main/java/com/ljj/easesim/controllers/SimHomeController.java
@@ -1,8 +1,10 @@
 package com.ljj.easesim.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ljj.easesim.commands.ToggleDoorCommand;
 import com.ljj.easesim.commands.ToggleLightCommand;
 import com.ljj.easesim.commands.ToggleWindowCommand;
+import com.ljj.easesim.elements.Door;
 import com.ljj.easesim.elements.Light;
 import com.ljj.easesim.elements.Window;
 import com.ljj.easesim.layout.HouseLayout;
@@ -68,6 +70,29 @@ public class SimHomeController {
         }
     }
 
+    @GetMapping("/getHouseDoors")
+    public String getHouseDoors() {
+        HouseLayout house = HouseLayout.getInstance();
+        List<Map<String, Object>> roomsList = new ArrayList<>();
+
+        house.getHouseDoors().forEach((key, value) -> {
+            Map<String, Object> room = new HashMap<>();
+            room.put("roomName", key);
+            room.put("door", value.getState());
+            room.put("id", value.getId());
+            roomsList.add(room);
+        });
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(roomsList);
+        } catch (Exception e) {
+            // Handle the error properly
+            e.printStackTrace();
+            return "Error converting to JSON";
+        }
+    }
+
     @PostMapping("/toggleLight")
     public String toggleLight(@RequestBody ToggleRequest request) {
         HouseLayout house = HouseLayout.getInstance();
@@ -110,5 +135,27 @@ public class SimHomeController {
         System.out.println(window.get().getState());
 
         return "Window state is: " + window.get().getState();
+    }
+
+    @PostMapping("/toggleDoor")
+    public String toggleDoor(@RequestBody ToggleRequest request) {
+        HouseLayout house = HouseLayout.getInstance();
+        AtomicReference<Room> room = new AtomicReference<Room>();
+        AtomicReference<Door> door = new AtomicReference<Door>();
+
+        house.getHouseDoors().forEach((key, value) -> {
+            if (value.getId() == request.getId()) {
+                room.set(house.getRoom(key));
+                door.set((Door) value);
+            }
+        });
+
+        // Toggle using Command Design Pattern
+        System.out.println(door.get().getState());
+        room.get().setCommand(new ToggleDoorCommand(door.get()));
+        room.get().executeCommand();
+        System.out.println(door.get().getState());
+
+        return "Door state is: " + door.get().getState();
     }
 }
