68dc38591388d0b1b66ce1f9bfa16bd7ab665513
andymiha <andreimihaescu@hotmail.com>
2024-03-17 14:35:17 -0400

    shc bracket fix

--- frontend/src/components/shc/shc.jsx
+++ frontend/src/components/shc/shc.jsx
@@ -4,36 +4,32 @@ import { useState, useEffect } from "react";
 
 const SHC = () => {
   const [lights, setLights] = useState([])
-const [doors, setDoors] = useState([])
-const [windows, setWindows] = useState([])
+  const [doors, setDoors] = useState([])
+  const [windows, setWindows] = useState([])
 
- // Fetch data from backend on mount
- useEffect(() => {  
-  fetch('http://localhost:8080/getHouseLights') 
-      .then(response => response.json())
-      .then(data => {
-         setLights(data)
-      })
-      .then(console.log("bluetooth connect assucessfully"))
-      .catch(error => console.error('Error fetching data:', error));
- },
-  []);
+  useEffect(() => {  
+    fetch('http://localhost:8080/getHouseLights') 
+        .then(response => response.json())
+        .then(data => {
+          setLights(data)
+        })
+        .then(console.log("bluetooth connect assucessfully"))
+        .catch(error => console.error('Error fetching data:', error));
+  },
+    []);
 
-  console.log(lights);
 
-   // Fetch data from backend on mount
- useEffect(() => {  
-  fetch('http://localhost:8080/getHouseWindows') 
-      .then(response => response.json())
-      .then(data => {
-         setWindows(data)
-      })
-      .then(console.log("bluetooth connect assucessfully"))
-      .catch(error => console.error('Error fetching data:', error));
- },
-  []);
+  useEffect(() => {  
+    fetch('http://localhost:8080/getHouseWindows') 
+        .then(response => response.json())
+        .then(data => {
+          setWindows(data)
+        })
+        .then(console.log("bluetooth connect assucessfully"))
+        .catch(error => console.error('Error fetching data:', error));
+  },
+    []);
 
-  console.log(windows);
 
   useEffect(() => {  
     fetch('http://localhost:8080/getHouseDoors') 
@@ -46,14 +42,13 @@ const [windows, setWindows] = useState([])
    },
     []);
   
-  //  console.log(doors);
 
   const [rows, setRows] = useState([]);
 
   const generateRows = (data) => {
     setRows(data.map((item) => ({
-      room: item.roomName || item.roomFrom,
-      roomTo: item.roomTo, 
+      id: item.id,
+      room: item.roomName,
       isOn: item.state,
       isBlocked: item.isBlocked || false,
       isAuto: item.isAuto || false,
@@ -66,7 +61,33 @@ const [windows, setWindows] = useState([])
     setAlignment(newAlignment);
   };
 
-  const handleSwitchChange = (index) => { 
+  const handleLightChange = (row, index) => { 
+    fetch('http://localhost:8080/toggleLight', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: row.id })
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Error toggling: ' + response.statusText);
+        }
+    })
+    .then(data => {
+        console.log(data);
+        setRows(prevRows => 
+            prevRows.map((row, i) =>
+                i === index ? {...row, isOn: data.state} : row
+            )
+        );
+    })
+    .catch(error => {
+        console.error('Error toggling:', error);
+    });
+};
 
 const handleDoorChange = (row, index) => { 
   fetch('http://localhost:8080/toggleDoor', {
@@ -144,9 +165,14 @@ const handleLightAutoChange = (row, index) => {
       console.log(data);
       setRows ((prevRows) => 
       prevRows.map((row, i) =>
-        i === index ? {...row, isOn: !row.isOn} : row
+        i === index ? {...row, isAuto: !row.isAuto} : row
       )
-    );
+      );
+  })
+  .catch(error => {
+      console.error('Error toggling:', error);
+  });
+    
   };
 
   const handleDoorAutoChange = (row, index) => {
@@ -171,7 +197,12 @@ const handleLightAutoChange = (row, index) => {
       prevRows.map((row, i) =>
         i === index ? {...row, isAuto: !row.isAuto} : row
       )
-    );
+      );
+  })
+  .catch(error => {
+      console.error('Error toggling:', error);
+  });
+    
   };
 
 
@@ -205,8 +236,7 @@ const handleLightAutoChange = (row, index) => {
             <TableHead>
               <TableRow>
            {/* isAlighnment == "center" */}
-              {alignment !== "center" && <TableCell>Room</TableCell>}
-              {alignment === "center" && <TableCell>Rooms</TableCell>}
+                 <TableCell>Room</TableCell>
                 <TableCell>On/Off</TableCell>
                 {alignment !== "right" && <TableCell>Auto-mode</TableCell>}
               </TableRow>
@@ -217,28 +247,39 @@ const handleLightAutoChange = (row, index) => {
                   key={index}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
-                  {alignment === "center" && (
-                  <TableCell>{row.room} to {row.roomTo}</TableCell>
-                   )}
-                  {alignment !== "center" &&( <TableCell>{row.room}</TableCell>
-                  )}
+                  <TableCell>{row.room}</TableCell>
                   <TableCell>
-                  <Switch
+                  {alignment === "left" && (<Switch
                     checked={row.isOn}
-                    onChange = {() => handleSwitchChange (index)}
+                    onChange={() => handleLightChange(row, index)}
                     disabled = {row.isBlocked || row.isAuto}
-                    />
+                    />)}
+                   {alignment === "center" && (<Switch
+                    checked={row.isOn}
+                    onChange={() => handleDoorChange(row, index)}
+                    disabled = {row.isBlocked || row.isAuto}
+                    />)}
+                  {alignment === "right" && (<Switch
+                    checked={row.isOn}
+                    onChange={() => handleWindowChange(row, index)}
+                    disabled = {row.isBlocked || row.isAuto}
+                    />)}
                     {row.isOn}</TableCell>
-                    {alignment !== "right" && (
+              
                   <TableCell>
-                  <Checkbox
+                  {alignment === "left" && (<Checkbox
                         checked={row.isAuto}
-                        onChange = {() => handleCheckboxChange (index)}
-                      />
-                    {row.isAuto}</TableCell>
-                      )}
+                        onChange = {() => handleLightAutoChange (row,index)}
+                      />)}
+                
+                   {alignment === "center" && (<Checkbox
+                        checked={row.isAuto}
+                        onChange = {() => handleDoorAutoChange (row,index)}
+                      />)}
+                {row.isAuto}
+                </TableCell>
                 </TableRow>
-              ))}
+                ))}
             </TableBody>
           </Table>
         </TableContainer>
@@ -249,41 +290,4 @@ const handleLightAutoChange = (row, index) => {
 }
 export default SHC;
 
-// export const Layout = () => {
-
-//   return (
-//     <Box>
-//       <Stack
-//          direction="column"
-//          justifyContent="center"
-//          alignItems="center"
-//          spacing={5}
-//       >
-//       <Typography variant="h5" >House Layout</Typography>
-//       <img
-//         src="src/assets/HouseLayout.png"
-//         alt="House Layout"
-//         style={{ 
-//         position: 'relative',
-//         maxWidth: '100%', 
-//         height: 'auto', 
-//         }}
-//       />
-//         {isOn && (
-//           <div
-//             style={{
-//               position: "absolute",
-//               top: "50px", // Adjust position as needed
-//               left: "100px", // Adjust position as needed
-//               width: "50px", // Adjust size as needed
-//               height: "50px", // Adjust size as needed
-//               backgroundColor: "rgba(255, 0, 0, 0.5)", // Semi-transparent red color
-//             }}
-//           >
-         
-//            </div>
-//           )}  
-//       </Stack>
-//     </Box>
-//   ); 
- //
+ 
