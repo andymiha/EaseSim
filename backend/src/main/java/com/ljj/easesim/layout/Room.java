a6305877b57b215d25c0065dafd9f1d94d3145d2
andymiha <andreimihaescu@hotmail.com>
2024-03-23 18:48:51 -0400

    fixed typo in toString()

--- backend/src/main/java/com/ljj/easesim/layout/Room.java
+++ backend/src/main/java/com/ljj/easesim/layout/Room.java
@@ -118,7 +118,7 @@ public class Room {
         return "Room{" +
                 "id=" + id +
                 ", command=" + command +
-                ", name='" + name + '\'' +
+                ", name=" + name  +
                 ", elements=" + elements +
                 ", users=" + users +
                 '}';
