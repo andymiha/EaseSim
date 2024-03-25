9cdbc1024d237acc3fd3374e5745069a5674c34e
nouratabbara <nouratabara@gmail.com>
2024-03-25 17:50:16 -0400

    created ToggleRequest and UserRequest tests

--- backend/src/main/java/com/ljj/easesim/requestBodies/ToggleRequest.java
+++ backend/src/main/java/com/ljj/easesim/requestBodies/ToggleRequest.java
@@ -2,7 +2,6 @@ package com.ljj.easesim.requestBodies;
 public class ToggleRequest {
     private int id;
     private int userId;
-    private boolean state;
 
     // Getters and setters
     public int getId() {
@@ -12,5 +11,12 @@ public class ToggleRequest {
         return userId;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
 
