9da2bdbd5e9df41725d74eb50b28ad717d9bb77b
andymiha <andreimihaescu@hotmail.com>
2024-04-07 16:14:22 -0400

    #21 created testing class for SHP
    Co-authored-by: Kahiso <mehdifouzail@gmail.com>
    Co-authored-by: nouratabbara <nouratabara@gmail.com>

---
+++ backend/src/test/java/com/ljj/easesim/SmartHomeSecurityTest.java
@@ -0,0 +1,52 @@
+package com.ljj.easesim;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmartHomeSecurityTest {
+
+    @Test
+    void getInstance() { 
+    }
+
+    @Test
+    void isAway() {
+    }
+
+    @Test
+    void registerAwayModeObserver() {
+    }
+
+    @Test
+    void removeAwayModeObserver() {
+    }
+
+    @Test
+    void notifyAwayModeObservers() {
+    }
+
+    @Test
+    void setAwayMode() {
+    }
+
+    @Test
+    void lockdown() {
+    }
+
+    @Test
+    void updateTemperature() {
+    }
+
+    @Test
+    void logEvent() {
+    }
+
+    @Test
+    void sendNotificationToOwners() {
+    }
+
+    @Test
+    void printIndoorTemps() {
+    }
+}
