8e5c5db0c54c1e7f8cd82f5e1753b6e1deb2332d
andymiha <andreimihaescu@hotmail.com>
2024-04-08 20:19:30 -0400

    added endpoints for frontend

--- backend/src/main/java/com/ljj/easesim/controllers/SHPController.java
+++ backend/src/main/java/com/ljj/easesim/controllers/SHPController.java
@@ -6,17 +6,27 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/shp")
 public class SHPController {
-    private final SmartHomeSecurity shs = SmartHomeSecurity.getInstance();
+    private final SmartHomeSecurity shp = SmartHomeSecurity.getInstance();
+
+    @GetMapping("/get-away-mode")
+    public boolean getAwayMode() {
+        return shp.isAway();
+    }
 
     @PostMapping("/set-away-mode")
     public String setAwayMode(@RequestBody boolean isAway) {
-        shs.setAwayMode(isAway);
+        shp.setAwayMode(isAway);
         return "Away mode set to: " + isAway;
+    }
+
+    @GetMapping("/get-police-timer")
+    public int getPoliceTimer() {
+        return shp.getPoliceTimer();
     }
 
     @PostMapping("/set-police-timer")
     public String setPoliceTimer(@RequestBody int policeTimer) {
-        shs.setPoliceTimer(policeTimer);
+        shp.setPoliceTimer(policeTimer);
         return "Police timer set to: " + policeTimer;
     }
 
